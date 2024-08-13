28ee5ea5c5d840c02a23d508531da1fb7f1205d2
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-08-14 01:29:22 +0530

    ownership test 2

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,17 @@
 fn main() {
-    let x:String = String::from("Hello"); // x is owner of Hello
-    process_string(x); // ownership transferred
-    // println!("Value of x remains, i.e. {}",x); // this will throw errors, as x is invalid here
+    let s1: String = get_string(); // s1 is the new owner of hello
+    println!("s1={}", s1);
+
+    let s2: String = String::from("sssstttrrriiinnnggg 222"); // s2 is owner of the given big string
+    let s3: String = send_get_string(s2); // transfer of ownership from s2 to arg, then arg to s3, s3 is new owner of big string
+    println!("s3={}", s3);
 }
 
-fn process_string(item:String) { // item is new owner of Hello
-    println!("The value of item is {}",item);
+fn get_string() -> String {
+    let str = String::from("hello"); //str owner of hello
+    return str; // transfer of ownership
 }
 
-//after the process_string the `item` becomes invalid, as rust removes it from memory when the scope is ended
-
-// here data types are static, so they work with stack, so no error, but upper example works with heaps and so ownership
-
-
-// fn main() {
-//     let x:u8 = 5;
-//     process_integer(x);
-//     println!("Value of x remains, i.e. {}",x);
-// }
-
-// fn process_integer(item:u8) {
-//     println!("The value of item is {}",item);
-// }
+fn send_get_string(arg: String) -> String { // arg is new owner of the big string
+    return arg; // transfer of ownership
+}
