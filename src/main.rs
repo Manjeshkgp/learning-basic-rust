a91e29071786d7b093e53560d8409b129cc8e895
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-08-14 01:15:49 +0530

    ownership test 1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 fn main() {
-    let str1 = String::from("Hello"); // str1 is owner of hello
-    let str2 = str1; // transfer of ownership, str2 is new owner, str1 becomes invalidated
-    // println!("str1 = {}",str1); // this will throw error, as str2 is the owner and str1 is invalid
-    println!("str2 = {}",str2);
+    let x:String = String::from("Hello"); // x is owner of Hello
+    process_string(x); // ownership transferred
+    // println!("Value of x remains, i.e. {}",x); // this will throw errors, as x is invalid here
 }
+
+fn process_string(item:String) { // item is new owner of Hello
+    println!("The value of item is {}",item);
+}
+
+//after the process_string the `item` becomes invalid, as rust removes it from memory when the scope is ended
+
+// here data types are static, so they work with stack, so no error, but upper example works with heaps and so ownership
+
+
+// fn main() {
+//     let x:u8 = 5;
+//     process_integer(x);
+//     println!("Value of x remains, i.e. {}",x);
+// }
+
+// fn process_integer(item:u8) {
+//     println!("The value of item is {}",item);
+// }
