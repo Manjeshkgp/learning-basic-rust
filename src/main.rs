685346a1e37e405bc54854b0d887ed2052aff034
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-08-14 01:56:37 +0530

    t3 solved by borrow operation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     let s1: String = String::from("Hello");
-    let len = calc_length(s1.clone()); 
+    let len = calc_length(&s1); // passing reference of s1 || borrow operation
     println!("The main string is {} and it's length is {}", s1, len);
 }
 
-fn calc_length(arg: String) -> usize { 
-    return arg.len();
+fn calc_length(arg: &String) -> usize { // arg refers to value of s1 but does not own it
+    return arg.len(); // after borrowing, no write operations can be done, only read operations allowed
 }
