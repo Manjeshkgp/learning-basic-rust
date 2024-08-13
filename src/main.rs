b3a6a1cb8bf3299ec3cff90b8ac5798e72ee3397
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-08-14 01:44:30 +0530

    t3 solved via cloning

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let s1: String = String::from("Hello"); // s1 owner
-    let (s2, len) = calc_length(s1); // ownership transfer from s1 to arg, and again arg to s2, also length transferred to len
-    println!("The main string is {} and it's length is {}", s2, len);
+    let s1: String = String::from("Hello");
+    let len = calc_length(s1.clone()); 
+    println!("The main string is {} and it's length is {}", s1, len);
 }
 
-fn calc_length(arg: String) -> (String, usize) { // arg will be new owner
-    let length: usize = arg.len(); // length is new owner of the string size
-    return (arg, length); // again ownership transferred
+fn calc_length(arg: String) -> usize { 
+    return arg.len();
 }
