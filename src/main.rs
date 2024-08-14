990eb54686401e806057562b6f965f7d94a3588f
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-08-15 00:32:13 +0530

    dereferencing p1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-fn main () {
-    let mut s1:String = String::from("string 1"); // s1 owner of mutable string string 1
-    let w1 = &mut s1; // reference of mutable s1 is w1 
-    w1.push_str(" add w1");
-    println!("w1 = {}",w1);
-    let w2 = &mut s1; // reference of mutable s1 is w2, so w1 is gone (because it is mutable i.e. write operation, for non mutable w1 still refers to s1 value)
-    w2.push_str(" add w2");
-    println!("w2 = {}",w2);
-    // println!("w1={}",w1); // error will be thrown, since w1 is invalidated already
-    println!("s1 = {}",&mut s1); // same as w2
+fn main() {
+    let x = 5;
+    let y = &x; // y is referencing to the value of x
+    println!("address of x = {:p}",&x); // getting same addresses
+    println!("address of y = {:p}",y); // getting same addresses
+
+    println!("y={}",y); // auto dereferncing = gives value of what y is referring to
 }
