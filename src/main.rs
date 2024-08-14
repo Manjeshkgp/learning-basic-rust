101bc7b8853885938f22fbf2193ffb76c3357251
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-08-15 01:10:07 +0530

    dangling referrence

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+// Dangling Referrence
+
 fn main() {
-    let mut x = 5;
-    x = x+1; // don't use dereferncing operator here, not worj
-    let y = &mut x; // y is referering to the value of mut x, and having address of mut x as it's value
-    // y=y+1; can't do this, as this will work with the address of x, which throws error
-    *y=*y+1; // that's why we used dereferencing operator
-    println!("x={}",y);
+    let a = String::from("hello");
+    let refer_to_nothing = give_me_reffer(a);
+}
+
+fn give_me_reffer()->&String {
+    let s = String::from("hello");
+    return &s; // scope of s is ended here that's why refer_to_nothing will be invalid, so this will throw error
 }
