db143bdc02c4c4e999923b876be4aaab8b0b82d9
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-08-15 00:04:18 +0530

    mutable referrence read write concept

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
-fn main() {
-    // Create a mutable String s1 with initial content "Hello"
-    let mut s1: String = String::from("Hello");
-    
-    // Borrow s1 as mutable and pass it to the append_string function
-    // This allows the function to modify the original String s1
-    append_string(&mut s1); 
-    
-    // After the function call, s1 has been mutated and now contains "Hello extra str"
-    println!("The new string s1 becomes {}", s1);
-}
-
-// This function takes a mutable reference to a String
-// The mutable reference allows the function to modify the String that was passed to it
-fn append_string(arg: &mut String) { 
-    // Mutate the borrowed String by appending " extra str" to it
-    arg.push_str(" extra str");
+fn main () {
+    let mut s1:String = String::from("string 1"); // s1 owner of mutable string string 1
+    let w1 = &mut s1; // reference of mutable s1 is w1 
+    w1.push_str(" add w1");
+    println!("w1 = {}",w1);
+    let w2 = &mut s1; // reference of mutable s1 is w2, so w1 is gone (because it is mutable i.e. write operation, for non mutable w1 still refers to s1 value)
+    w2.push_str(" add w2");
+    println!("w2 = {}",w2);
+    // println!("w1={}",w1); // error will be thrown, since w1 is invalidated already
+    println!("s1 = {}",&mut s1); // same as w2
 }
