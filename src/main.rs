527ef07a87f60631c34d71b5cf2e4d4efec0665d
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-08-16 15:46:20 +0530

    basic IO

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,9 @@
-// match
-
-// fn main(){
-//     let number = 107;
-
-//     match number {
-//         1|3=> println!("Number is One or Three"),
-//         2|4=> println!("Number is Two or Four"),
-//         5|7=> println!("Number is Five or Seven"),
-//         _=>println!("Number is not recognisable")
-//     }
-// }
-
+use std::io;
 fn main() {
-    fn is_even(num: isize) -> bool {
-        // if num % 2 == 0 {   this also works
-        //     return true;
-        // } else {
-        //     return false;
-        // }
-        return num % 2 == 0;
-    }
-    let number = 20;
-    match number {
-        x if is_even(x) => println!("Even"),
-        x if !is_even(x) => println!("Odd"),
-        _ => println!("Number not recognizable"),
-    }
+    let mut input = String::new();
+    println!("Please enter your name:");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Expect Failed");
+    println!("Use input: {}", input);
 }
