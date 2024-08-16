0a158df8303144282c3434b4e87bfbc3e5509344
Manjesh Kumar Sharma <manjeshkgp@gmail.com>
2024-08-16 14:36:42 +0530

    match like switch

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
-// loops
+// match
 
-// fn main() {
-//     let mut i = 0;
-//     while i<=5 {
-//         println!("{}",i);
-//         i+=1;
+// fn main(){
+//     let number = 107;
+
+//     match number {
+//         1|3=> println!("Number is One or Three"),
+//         2|4=> println!("Number is Two or Four"),
+//         5|7=> println!("Number is Five or Seven"),
+//         _=>println!("Number is not recognisable")
 //     }
 // }
 
-// fn main() {
-//     loop {
-//         println!("Infinite Printing");
-//     }
-// }
-
-fn main () {
-    let arr = [1,2,3];
-    for element in &arr{
-        println!("element = {}",element);
+fn main() {
+    fn is_even(num: isize) -> bool {
+        // if num % 2 == 0 {   this also works
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+        return num % 2 == 0;
+    }
+    let number = 20;
+    match number {
+        x if is_even(x) => println!("Even"),
+        x if !is_even(x) => println!("Odd"),
+        _ => println!("Number not recognizable"),
     }
 }
